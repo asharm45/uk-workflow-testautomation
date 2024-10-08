303ee40f541fd362812748db3191a2fbca387a8a
Amit Sharma <amits5792@gmail.com>
2024-10-08 20:52:47 +0530

    Add files via upload

---
+++ WorkflowSpecflowTests/Models/Sample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowSpecflowTests.Models
+{
+    public class Sample
+    {
+        public bool Product { get; set; }
+        public bool Task { get; set; }
+        public float Case { get; set; }
+
+    }
+}
